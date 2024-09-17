3875705ab9b3c07c4a384a851fa2de23362a7085
Sunil Kumar <sunil.kumar@toolstation.com>
2024-09-17 13:26:28 +0530

    Updated: Setup routes

--- Todo/Todo-Frontend/src/router/index.js
+++ Todo/Todo-Frontend/src/router/index.js
@@ -1,23 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import TodoList from '../components/TodoList.vue';
+import TodoForm from '../components/TodoForm.vue';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+const routes = [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+        path: '/',
+        name: 'Home',
+        component: TodoList,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+        path: '/create',
+        name: 'CreateTodo',
+        component: TodoForm,
+    },
+    {
+        path: '/edit/:id',
+        name: 'EditTodo',
+        component: TodoForm,
+    },
+];
 
-export default router
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
