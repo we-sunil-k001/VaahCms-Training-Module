f21a6f68c1723ac49c5835aee6318775f87338d8
Sunil Kumar <sunil.kumar@toolstation.com>
2024-09-17 08:28:05 +0530

    Added: TodoController

---
+++ Todo/app/Http/Controllers/TodoController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    /**
+     * Fetch all Todos
+     */
+    public function index()
+    {
+        return Todo::all();
+    }
+
+    /**
+     * Fetch single Todos
+     */
+    public function show(Todo $todo)
+    {
+        return $todo;
+    }
+
+
+    /**
+     * Create a new Todo
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=> 'required|unique:todos,name',
+            'description' => 'required',
+        ]);
+
+        return Todo::create($request->all());
+    }
+
+    /**
+     * Update the specified Todo
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'name' => 'sometimes|required|unique:todos,name,' . $todo->id,  //this validates name - unique from other existing rows except its own row **$todo->id**
+            'description' => 'sometimes|required',
+        ]);
+
+        $todo->update($request->all());
+        return $todo;
+    }
+
+    /**
+     * Mark task as completed
+     */
+    public function markComplete(Todo $todo)
+    {
+        $todo->update(['is_completed'=> true]);
+        //dd($todo);
+        return $todo;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return response()->json(['message' => 'Todo deleted successfully']);
+    }
+}
