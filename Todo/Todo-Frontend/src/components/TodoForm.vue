05356c1fbdfe14eac7abd7846ed1cf7e6541665b
Sunil Kumar <sunil.kumar@toolstation.com>
2024-09-18 18:54:10 +0530

    Updated: few variables as per naming conventions

--- Todo/Todo-Frontend/src/components/TodoForm.vue
+++ Todo/Todo-Frontend/src/components/TodoForm.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="container mt-4">
-        <h2>{{ isEditing ? 'Edit Task' : 'Create a new Task' }}</h2>
+        <h2>{{ is_editing ? 'Edit Task' : 'Create a new Task' }}</h2>
         <form @submit.prevent="submitForm">
             <div class="mb-3">
                 <label for="name" class="form-label">Name:</label>
                 <input type="text" class="form-control" v-model="name" />
-                <div v-if="nameError" class="text-danger">{{ nameError }}</div>
+                <div v-if="name_error" class="text-danger">{{ name_error }}</div>
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Description:</label>
                 <textarea class="form-control" v-model="description" ></textarea>
-                <div v-if="descriptionError" class="text-danger">{{ descriptionError }}</div>
+                <div v-if="description_rror" class="text-danger">{{ description_rror }}</div>
             </div>
-            <button type="submit" class="btn btn-primary">{{ isEditing ? 'Update Todo' : 'Create Todo' }}</button>
+            <button type="submit" class="btn btn-primary">{{ is_editing ? 'Update Todo' : 'Create Todo' }}</button>
         </form>
     </div>
 </template>
@@ -26,16 +26,16 @@ const store = useTodoStore();
 const route = useRoute();
 const router = useRouter();
 
-const isEditing = ref(false);
+const is_editing = ref(false);
 const name = ref('');
 const description = ref('');
 
-const nameError = ref('');
-const descriptionError = ref('');
+const name_error = ref('');
+const description_rror = ref('');
 
 onMounted(() => {
     if (route.params.id) {
-        isEditing.value = true;
+        is_editing.value = true;
         const todo = store.todos.find(todo => todo.id === parseInt(route.params.id));
         if (todo) {
             name.value = todo.name;
@@ -45,8 +45,8 @@ onMounted(() => {
 });
 
 const validateForm = () => {
-    nameError.value = '';
-    descriptionError.value = '';
+    name_error.value = '';
+    description_rror.value = '';
 
     // Trim whitespace from the input values
     const trimmedName = name.value.trim();     // trim(): Removes whitespace from both ends of the name.value string
@@ -54,13 +54,13 @@ const validateForm = () => {
 
     // Validate name
     if (!trimmedName) {     // here trimmedName is after removing whitespaces
-        nameError.value = 'Name is required';
+        name_error.value = 'Name is required';
         return false;
     }
 
     // Validate description
     if (!trimmedDescription) {
-        descriptionError.value = 'Description is required';
+        description_rror.value = 'Description is required';
         return false;
     }
 
@@ -69,7 +69,7 @@ const validateForm = () => {
 
 const submitForm = () => {
     if (validateForm()) {
-        if (isEditing.value) {
+        if (is_editing.value) {
             store.editTodo(route.params.id, { name: name.value, description: description.value });
         } else {
             store.createTodo({ name: name.value, description: description.value });
